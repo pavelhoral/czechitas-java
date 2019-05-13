fc3f006e0fc283634f347f68d067010c75b02ee5
Pavel Horal <pavel.horal@orchitech.cz>
2019-05-13 11:52:53 +0200

    Rozdeleni workspace.

--- code.cmd
+++ code.cmd
@@ -2,6 +2,8 @@ echo %~dp0
 @echo off
 setlocal
 set JAVA_HOME=%~dp0jdk-12.0.1
-set MAVEN_CONFIG=-Dmaven.repo.local=%~dp0maven
-start "Visual Studio Code" "%~dp0vscode-1.33.1\Code.exe" --extensions-dir extensions workspace
+set MAVEN_CONFIG=-Dmaven.repo.local=%~dp0maven\repository
+set MVNW_PATH=%~dp0maven\mvnw.cmd
+start "Visual Studio Code" "%~dp0vscode-1.33.1\Code.exe" --extensions-dir extensions --user-data-dir .vscode workspace/10-objekty
 endlocal
+
