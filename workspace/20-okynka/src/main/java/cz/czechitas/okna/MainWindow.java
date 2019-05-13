b42d2af3afca970a8308eef3fc5665b742fd9083
Pavel Horal <pavel.horal@orchitech.cz>
2019-05-13 22:39:35 +0200

    okynka-04

--- workspace/20-okynka/src/main/java/cz/czechitas/okna/MainWindow.java
+++ workspace/20-okynka/src/main/java/cz/czechitas/okna/MainWindow.java
@@ -1,7 +1,9 @@
 package cz.czechitas.okna;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -11,6 +13,8 @@ import javax.swing.JFrame;
 @SuppressWarnings("serial")
 public class MainWindow extends JFrame {
 
+    private MouseHandler mouseHandler = new MouseHandler();
+
     public void initialize() {
         setTitle("Czechitas");
         setSize(400, 400);
@@ -18,21 +22,60 @@ public class MainWindow extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
-        addMouseListener(new MouseHandler());
+        addMouseListener(mouseHandler);
+        addMouseMotionListener(mouseHandler);
     }
     
-    private void makej(Point point) {
-        Graphics graphics = getGraphics();
-        graphics.setColor(Color.BLUE);
-        graphics.fillOval(point.x, point.y, 10, 10);
+    private void makej(Point start, Point end) {
+        Graphics2D graphics = (Graphics2D) getGraphics();
+        graphics.setColor(Color.RED);
+        graphics.setStroke(new BasicStroke(5, BasicStroke.CAP_ROUND, 0));
+        graphics.drawLine(start.x, start.y, end.x, end.y);
+    }
+
+    private void smazto() {
+        Graphics2D graphics = (Graphics2D) getGraphics();
+        Dimension size = getSize();
+        graphics.clearRect(0, 0, size.width, size.height);
     }
 
 
     private class MouseHandler extends MouseAdapter {
 
+        private Point previous;
+
         @Override
-        public void mouseClicked(MouseEvent e) {
-            makej(e.getPoint());
+        public void mousePressed(MouseEvent e) {
+            if (e.getButton() == MouseEvent.BUTTON1) {
+                previous = e.getPoint();
+            } else {
+                smazto();
+            }
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            previous = null;
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            if (previous == null) {
+                return;
+            }
+            Point current = e.getPoint();
+            makej(previous, current);
+            previous = current;
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            previous = null;
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            previous = e.getPoint();
         }
 
     }
